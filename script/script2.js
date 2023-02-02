59d8d7d47190ca761becf9320fc32ee04cb61e16
Cedric Chatellier <cedricchatellier95@gmail.com>
2023-02-02 12:00:30 +0100

    ajusted visual

--- script/script2.js
+++ script/script2.js
@@ -1,12 +1,13 @@
-
+document.querySelector('#menu').style.backgroundImage= 'url("./img/boite.jpeg")';
 const startBtn= document.querySelector('#start');
 startBtn.addEventListener('click', ()=> {
     createGame();
     document.querySelector('#menu').remove();
-})
+    document.querySelector('h1').style.visibility= 'visible';
+});
 
 function createGame(){
-    const players= ['Colonel Moutarde', 'Madame Pervenche', 'Mademoiselle Rose', 'Professeur Violet', 'Madame Leblanc', 'Docteur Olive'];
+    const players= ['Colonel Moutarde', 'Madame Pervenche',  'Madame Leblanc', 'Mademoiselle Rose', 'Docteur Olive', 'Professeur Violet'];
     const rooms= ['Kitchen', 'Dining room', 'Living room','Lounge', 'Hall','Winter Garden', 'Billiard room', 'Library', 'Office room'];
     const wepons= ['Candlestick', 'Wrench', 'Rope', 'Knife', 'Lamp', 'Hammer', 'Baton', 'Poison', 'Revolver'];
     let game= new Game(players, rooms, wepons);
@@ -17,53 +18,67 @@ function createGame(){
 
 function makeUlInSection (){
     const sectionElement= document.querySelector('#game');
-    
-    
-    const divElement= document.createElement("div");
-    divElement.classList.add('game');
-    sectionElement.append(divElement);
-    
 
-    const ulPlayers= document.createElement('ul');
+    const divPlay= document.createElement('div');
+    const divPlayers= document.createElement('div');
+    divPlay.classList.add('container');
     const h2Players= document.createElement('h2');
-    ulPlayers.append(h2Players);
-    ulPlayers.classList.add('players');
+    // divPlay.append(ulPlayers);
+    divPlay.append(h2Players);
+    divPlay.append(divPlayers);
+    divPlayers.classList.add('players');
     h2Players.textContent= "PLAYERS :";
     
-    const ulRooms= document.createElement('div');
-    const h2Rooms= document.createElement('h2');
     const plateElement= document.createElement('div');
+    const h2Rooms= document.createElement('h2');
+    const divRoomElement= document.createElement('div');
+    divRoomElement.classList.add('container');
     const column1Plate= document.createElement('div');
     const column2Plate= document.createElement('div');
     const column3Plate= document.createElement('div');
     plateElement.append(column1Plate,column2Plate,column3Plate);
-    ulRooms.append(h2Rooms, plateElement);
+    divRoomElement.append(h2Rooms);
+    divRoomElement.append(plateElement);
     plateElement.classList.add('rooms');
     h2Rooms.textContent= "ROOMS :";
     
-    const ulWepons= document.createElement('ul');
-    const h2Wepons= document.createElement('h2');
     const divWepons= document.createElement('div');
-    divElement.append(ulWepons);
-    ulWepons.append(h2Wepons);
-    ulWepons.classList.add('wepons');
-    h2Wepons.textContent= "WEPONS :";
-    
+    divWep= document.createElement('div');
+    divWep.classList.add('container');
+    const h2Wepons= document.createElement('h2');
+    divWep.append(h2Wepons);
+    divWepons.classList.add('wepons');
+    h2Wepons.textContent= "WEAPONS :";
+    divWep.append(divWepons);
+
+    // const divUp= document.createElement('div');
+    const divGame= document.createElement('div');
+    divGame.classList.add('up');
+    divGame.append(divWep,divPlay,divRoomElement);
+
     const submitBtn= document.createElement('button');
     submitBtn.classList.add('submit');
     submitBtn.textContent= "Submit";
-    const divPlay= document.createElement('div');
+    const submitElem= document.createElement('div');
+    const counterElement= document.createElement('h2');
+    counterElement.classList.add('counter');
+    counterElement.textContent= 0;
+    counterdiv= document.createElement('div');
+    submtDiv= document.createElement('div');
+    submtDiv.classList.add('andReplay')
+    counterdiv.append(counterElement);
+    submtDiv.append(submitBtn)
+    submitElem.append(counterdiv);
+    submitElem.append(submtDiv);
 
-    const consoleElement= document.createElement("ol");
 
-    divPlay.append(ulPlayers);
-    divElement.append( divWepons, divPlay, ulRooms);
-    divElement.style.display="flex";
 
-    const submitElem= document.querySelector('#submit');
-    submitElem.append(submitBtn);
-    const resultElem= document.createElement('ol');
-    sectionElement.append(resultElem);
+    const divElement= document.createElement("div");
+    divElement.classList.add('game');
+    sectionElement.append(divElement);
+    divElement.append( divGame, submitElem);
+
+    
 }
 
 function updateBtnSubmit(game){
@@ -74,11 +89,9 @@ function updateBtnSubmit(game){
     }
 }
 
-function innocentClassforRooms(el, game){
-    const rooms = game.rooms.map(room=> room.name);
-    console.log(wepons);
+function innocentClassforRooms(el){
     for(let key in el.selectedRooms){
-        if((rooms.includes(key)&&el.selectedWepons[key].length===6) ||el.selectedRooms[key].length===9){
+        if(el.selectedRooms[key].length===9){
             document.querySelector(`#${key}`).classList.toggle('innocentfs');
         }
     }
@@ -92,10 +105,25 @@ function innocentClassforRooms(el, game){
                 }
             }
 }
-function innocentClassforWepons(el, game){
-    const wepons = game.wepons.map(wep=> wep.name);
+function innocentClassforRoomsforWepon(el){
+    for(let key in el.selectedRooms){
+        if(el.selectedRooms[key].length===6){
+            document.querySelector(`#${key}`).classList.toggle('innocentfs');
+        }
+    }
+    const selectR= document.querySelector('.rooms div .selected');
+            if(selectR){
+                
+                if(el.selectedRooms[selectR.getAttribute('id')]){
+                    el.selectedRooms[selectR.getAttribute('id')].forEach((other) =>{
+                        document.querySelector(`#${other.getId()}`).classList.toggle('innocent');
+                    })
+                }
+            }
+}
+function innocentClassforWepons(el){
     for(let key in el.selectedWepons){
-            if((wepons.includes(key)&&el.selectedWepons[key].length===6) ||el.selectedWepons[key].length===9){
+            if(el.selectedWepons[key].length===9){
                 document.querySelector(`#${key}`).classList.toggle('innocentfs');
             }
     }
@@ -107,6 +135,21 @@ function innocentClassforWepons(el, game){
                     })
                 }
             }
+}
+function innocentClassforWeponsforRoom(el){
+    for(let key in el.selectedWepons){
+        if(el.selectedWepons[key].length===6){
+            document.querySelector(`#${key}`).classList.toggle('innocentfs');
+        }
+}
+const selectW= document.querySelector('.wepons >.selected');
+        if(selectW){
+            if(el.selectedWepons[selectW.getAttribute('id')]){
+                el.selectedWepons[selectW.getAttribute('id')].forEach((other) =>{
+                    document.querySelector(`#${other.getId()}`).classList.toggle('innocent');
+                })
+            }
+        }
 }
 function innocentClassforPlayers(el){
     for(let key in el.selectedPlayers){
@@ -136,7 +179,6 @@ function roomsInUl(game){
             document.querySelector('.rooms> div:nth-child(3)').append(divElement);
         }
         divElement.setAttribute('id', room.getId());
-        divElement.textContent= room.name;
         divElement.style.backgroundImage = `url('./img/${room.getId()}.png')`;
         divElement.classList.add('room');
         divElement.addEventListener('click', ()=> {
@@ -149,24 +191,21 @@ function roomsInUl(game){
             document.querySelector('.rooms').classList.toggle('done');
             
             innocentClassforPlayers(room);
-            innocentClassforWepons(room, game);
+            innocentClassforWeponsforRoom(room);
             updateBtnSubmit(game);
         });
     });
 }
+
 function weponsInUl(game){
     game.wepons.forEach((wep)=> {
-        console.log(wep.getId(), `./img/${wep.getId()}.png`);
-        const li= document.createElement('li');
+        const li= document.createElement('div');
         document.querySelector('.wepons').append(li);
         li.setAttribute('id', wep.getId());
         li.classList.add('wepon');
         const imgElement= document.createElement('img');
         imgElement.src= `./img/${wep.getId()}.png`;
         li.append(imgElement);
-        const span= document.createElement('span');
-        li.append(span);
-        span.textContent= wep.name;
             li.addEventListener('click', ()=> {
                 if(!game.selection.includes(wep)){
                     game.selectWepon(wep);
@@ -177,23 +216,21 @@ function weponsInUl(game){
                 li.parentElement.classList.toggle('done');
 
                 innocentClassforPlayers(wep);
-                innocentClassforRooms(wep, game);
+                innocentClassforRoomsforWepon(wep);
                 updateBtnSubmit(game);
             }); 
     });
 }
+
 function playersInUL(game){
     game.players.forEach((player)=> {
-        const li= document.createElement('li');
-        document.querySelector('.players').append(li);
+        const li= document.createElement('div');
         li.setAttribute('id', player.getId());
         li.classList.add('player');
         const imgElement= document.createElement('img');
         imgElement.src= `./img/${player.getId()}.jpg`;
         li.append(imgElement);
-        const span= document.createElement('span');
-        li.append(span);
-        span.textContent= player.name;
+        document.querySelector('.players').append(li);
         li.addEventListener('click', ()=> {
             if(!game.selection.includes(player)){
                 game.selectPlayer(player);
@@ -202,13 +239,14 @@ function playersInUL(game){
             }
             li.classList.toggle('selected');
             li.parentElement.classList.toggle('done');
-            
-            innocentClassforRooms(player, game);
-            innocentClassforWepons(player, game);
+
+            innocentClassforRooms(player);
+            innocentClassforWepons(player);
             updateBtnSubmit(game);
         });
     });
 }
+
 function submitInUl(game){
     const submitBtn= document.querySelector('.submit');
     submitBtn.addEventListener('click', ()=> {        
@@ -218,14 +256,26 @@ function submitInUl(game){
         document.querySelectorAll('.innocentfs').forEach((el)=> el.classList.remove('innocentfs')); 
         submitBtn.classList.remove('open');
         console.log(game.selection);
-        const conLiElement= document.createElement('li');
-        document.querySelector('ol').append(conLiElement);
-        conLiElement.textContent= `Attemps ${game.nbAttemps}: No is not ${game.selection[0].name} in the ${game.selection[1].name} with the ${game.selection[2].name}`;
-        game.nbAttemps++;
         if (game.isItTrue()){
-            conLiElement.textContent= `Attemps ${game.nbAttemps}: It is true that ${game.selection[0].name} in the ${game.selection[1].name} with the ${game.selection[2].name}`;
-
+            game.nbAttemps= "win";
+            document.querySelector('#affiche').textContent= `Yes it was ${game.selection[0].name} with the ${game.selection[1].name} in the ${game.selection[2].name}!`;
+            document.querySelector('#affiche').style.visibility= 'visible';
+            const replayBtn = document.createElement('button');
+            replayBtn.textContent= 'Replay';
+            document.querySelector('.andReplay').append(replayBtn);
+            replayBtn.addEventListener('click', ()=> {
+                console.log('again');
+                document.querySelector('.game').remove();
+                createGame();
+            });
+        }else {
+            game.nbAttemps++;
+            setTimeout(()=>document.querySelector('#affiche').style.visibility= 'hidden', 10000);
+            document.querySelector('#affiche').textContent= `It is not ${game.selection[0].name} with the ${game.selection[1].name} in the ${game.selection[2].name}...`;
+            document.querySelector('#affiche').style.visibility= 'visible';
         }
+        document.querySelector('.counter').textContent= game.nbAttemps;
+        
         game.selection=[' ', ' ', ' '];
     });
 }
